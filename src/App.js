c3f319fcae4c049e996c23361fe7a6f9c1daef4e
Laura Junco <lc.junco10@uniandes.edu.co>
2021-01-12 10:04:04 -0500

    Fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Feel free to edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    error: null,
+    isLoaded: false,
+    data: []
+  };
+
+  componentDidMount() {
+    fetch("https://demo6922545.mockable.io")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result.data)
+          this.setState({
+            isLoaded: true,
+            data: result.data
+          });
+        },
+        
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>
+          Open targets technical test: Laura Junco
+        </p>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
