9485e72c55547225ae048d8900c6e23d76b2e5b7
Laura Junco <lc.junco10@uniandes.edu.co>
2021-01-12 09:42:32 -0500

    Updates

---
+++ precache-manifest.891d4766c05bc5b0501c7bde1361a50a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1189da2a8c4876ec90300fa2c16c9323",
+    "url": "/ot-front-end-technical-test/index.html"
+  },
+  {
+    "revision": "8a1bf304c9f55790d2f9",
+    "url": "/ot-front-end-technical-test/static/css/main.f0936c25.chunk.css"
+  },
+  {
+    "revision": "fbc306b9e99099f4c6c6",
+    "url": "/ot-front-end-technical-test/static/js/2.79a3c9ec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ot-front-end-technical-test/static/js/2.79a3c9ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a1bf304c9f55790d2f9",
+    "url": "/ot-front-end-technical-test/static/js/main.d335e6a5.chunk.js"
+  },
+  {
+    "revision": "728bc62a4c2d80560bb1",
+    "url": "/ot-front-end-technical-test/static/js/runtime-main.b1dc716d.js"
+  }
+]);
