94eaa657bb2d589315b7bf19fbfa8bedf06c7836
Laura Junco <lc.junco10@uniandes.edu.co>
2021-01-14 19:21:27 -0500

    Updates

---
+++ precache-manifest.35642f0570de3c7bd45306f4ba88d8f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4ea27ef5fbd8dca5feba14b705fd97",
+    "url": "/ot-front-end-technical-test/index.html"
+  },
+  {
+    "revision": "c704653d827fbf436528",
+    "url": "/ot-front-end-technical-test/static/css/main.e97af801.chunk.css"
+  },
+  {
+    "revision": "2d3fc0aaee65275fbdf9",
+    "url": "/ot-front-end-technical-test/static/js/2.c14aa385.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/ot-front-end-technical-test/static/js/2.c14aa385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c704653d827fbf436528",
+    "url": "/ot-front-end-technical-test/static/js/main.57abd441.chunk.js"
+  },
+  {
+    "revision": "728bc62a4c2d80560bb1",
+    "url": "/ot-front-end-technical-test/static/js/runtime-main.b1dc716d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ot-front-end-technical-test/precache-manifest.846dc5aceff2db25af858b12d26796f5.js"
+  "/ot-front-end-technical-test/precache-manifest.35642f0570de3c7bd45306f4ba88d8f2.js"
 );
 
 self.addEventListener('message', (event) => {
